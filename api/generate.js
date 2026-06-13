5dd5be4a4442591fd54734dc9662abd66491c94d
pjem20 <pjem2020@gmail.com>
2026-06-12 21:50:33 -0400

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -28,7 +28,7 @@ module.exports = async (req, res) => {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "openrouter/owl-alpha",
+          model: "poolside/laguna-m.1:free",
           messages: [
             {
               role: "system",
